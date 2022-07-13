7db68b0fcd19c9514e1a32303ac413045396e6c8
tiandreia <tiandreiarocha@gmail.com>
2022-07-13 12:33:11 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,20 @@
-import NotificationButtom from "./components/NotificationButtom"
+import Header from "./components/Header"
+import SalesCard from "./components/SalesCard"
+
 
 function App() {
   return (
     <>
-      <NotificationButtom />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
     </>
   )
 }
 
-export default NotificationButtom
+export default App
---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import logo from '../../assets/img/logo.svg'
+import './styles.css'
+
+
+function Header() {
+  return (
+    <header>
+      <div className="dsmeta-logo-container">
+        <img src={logo} alt="logo-dsmeta" />
+        <h1>DSMeta</h1>
+        <p>
+          Desenvolvido por
+          <a href="instagram.com/tiandreia">tiandreia</a>
+        </p>
+      </div>
+    </header>
+  )
+}
+
+export default Header
